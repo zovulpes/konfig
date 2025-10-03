3822a5f4ec8e647aa2582f0be817b3bb437d37f8
zovulpes <pastukhova_06@mail.ru>
2025-10-04 00:36:45 +0300

    Этап 2: Конфигурация
    - Добавлена обработка аргументов командной строки
    - Параметры: --vfs-path, --script, --prompt
    - Отладочный вывод параметров при запуске
    - Обновленное приглашение REPL

--- 1_stage_repl.py
+++ 1_stage_repl.py
@@ -1,12 +1,41 @@
 import getpass # для получения имени пользователя
 import socket  # для получения hostname
 import shlex   # для команды split - разделение строки на токены
+import sys     # для доступа к аргументам командной строки
 
 hostname = socket.gethostname()
 username = getpass.getuser()
 prompt = f"{username}@{hostname}:~$ "
 
 supported_commands =  {"ls", "cd", "exit"}
+
+# Парсим аргументы командной строки
+vfs_path = None    # путь к Виртуальной Файловой Системе
+script_path = None # путь к стартовому скрипту
+
+# sys.argv - это список всего что написали в командной строке
+# Например: ['1_repl.py', '--vfs-path', 'C:\data', '--script', 'start.txt']
+
+# Проходим по всем аргументам (кроме первого - имени файла)
+i = 1
+while i < len(sys.argv):
+    # Если видим "--vfs-path" и после него ЕСТЬ еще один аргумент
+    if sys.argv[i] == "--vfs-path" and i + 1 < len(sys.argv):
+        vfs_path = sys.argv[i + 1]    # Берем следующий аргумент
+        i += 2                        # Перескакиваем через два аргумента
+
+     # Если видим "--script" и после него ЕСТЬ еще один аргумент 
+    elif sys.argv[i] == "--script" and i + 1 < len(sys.argv):
+        script_path = sys.argv[i + 1] # Берем следующий аргумент
+        i += 2                        # Перескакиваем через два аргумента
+    else:
+        i += 1                        # Переходим к следующему аргументу
+
+# Отладочный вывод параметров
+print("=== Параметры эмулятора ===")
+print(f"VFS путь: {vfs_path}")
+print(f"Скрипт: {script_path}")
+print("===========================")
 
 while True:
     try:
